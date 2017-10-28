d897cae9c674936626af8f545bbfac29032c1cb2
simoniz0r <bnu.fantasma@gmail.com>
2017-10-28 05:42:03 -0500

    Clean up help output

--- wrapt.sh
+++ wrapt.sh
@@ -8,26 +8,26 @@ printf "
 wrapt - http://www.simonizor.gq
 A simple wrapper for apt that brings all useful apt commands into one easy to use script
 
-wrapt      - Show this help output (alias: wrapt -help)
-wrapt -l   - apt list - list packages based on package names
-wrapt -li  - dpkg --get-selections | grep -v deinstall - list all installed packages
-wrapt -S   - dpkg -S - show which package a file belongs to
-wrapt -L   - dpkg -L - list files installed by a package
-wrapt -sd  - apt-cache depends - list dependencies of a package
-wrapt -srd - apt-cache rdepends - list reverse dependencies of a package
-wrapt -se  - apt search - search in package descriptions
-wrapt -sh  - apt show - show package details
-wrapt -m   - apt -mark - simple command line interface for marking packages as manually or automatically installed
-wrapt -i   - apt install - install packages
-wrapt -deb - dpkg -i || apt install -f - install deb package and run apt install -f to install dependencies
-wrapt -r   - apt remove - remove packages
+wrapt      - Show this help output (alias: wrapt -h)
+wrapt -l   - apt list - List packages based on package names
+wrapt -li  - dpkg --get-selections | grep -v deinstall - List all installed packages
+wrapt -S   - dpkg -S - Show which package a file belongs to
+wrapt -L   - dpkg -L - List files installed by a package
+wrapt -sd  - apt-cache depends - List dependencies of a package
+wrapt -srd - apt-cache rdepends - List reverse dependencies of a package
+wrapt -se  - apt search - Search in package descriptions
+wrapt -sh  - apt show - Show package details
+wrapt -m   - apt-mark - Simple command line interface for marking packages as manually or automatically installed
+wrapt -i   - apt install - Install packages
+wrapt -deb - dpkg -i || apt install -f - Install deb package and run apt install -f to install dependencies
+wrapt -r   - apt remove - Remove packages
 wrapt -ra  - apt autoremove - Remove automatically all unused packages
-wrapt -ud  - apt update - update list of available packages
-wrapt -ug  - apt upgrade - upgrade the system by installing/upgrading packages
-wrapt -uu  - apt update && apt upgrade - run apt update and then apt upgrade
-wrapt -fu  - apt full-upgrade - fully upgrade the system by removing/installing/upgrading packages
-wrapt -ar  - apt-add-repository - wrapt -add-repository is a script for adding apt sources.list entries
-wrapt -es  - apt edit-sources - edit the source information file
+wrapt -ud  - apt update - Update list of available packages
+wrapt -ug  - apt upgrade - Upgrade the system by installing/upgrading packages
+wrapt -uu  - apt update && apt upgrade - Run apt update and then apt upgrade
+wrapt -fu  - apt full-upgrade - Fully upgrade the system by removing/installing/upgrading packages
+wrapt -ar  - apt-add-repository - A script for adding apt sources.list entries
+wrapt -es  - apt edit-sources - Edit the source information file
 "
 }
 
